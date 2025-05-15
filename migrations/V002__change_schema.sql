0db9394383c8392f496b9493606aad1aeaf9edc0
Барановский <nbaranovskii@astralinux.ru>
2025-05-15 19:59:34 +0300

    hotfix

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -3,11 +3,15 @@ DROP TABLE IF EXISTS product_info;
 DROP TABLE IF EXISTS orders_date;
 
 ALTER TABLE product
-    ADD PRIMARY KEY (id),
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE product
     ADD COLUMN price DOUBLE PRECISION;
 
 ALTER TABLE orders
-    ADD PRIMARY KEY (id),
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE orders
     ADD COLUMN date_created DATE NOT NULL;
 
 ALTER TABLE order_product
